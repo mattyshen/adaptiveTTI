7bef13ab4dddbfe082147747ab84eeff14f57376
mattyshen <mattyshen@berkeley.edu>
2025-01-03 00:04:15 -0800

    remove old folder

--- interpretDistill/__init__.py
+++
@@ -1,5 +0,0 @@
-from .fourierDistill import *
-from .tabdl import *
-
-from .model import *
-from .data import *
--- interpretDistill/continuous.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def is_continuous(feature):
-    return (pd.api.types.is_float_dtype(feature) and len(feature.unique()) > 12) or (pd.api.types.is_integer_dtype(feature) and len(feature.unique()) > 12)
